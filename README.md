5ab84dd1826608297dffdc0dea39c34f7603049f
danielaparker <danielaparker@yahoo.com>
2019-02-12 16:16:46 -0500

    README update

--- README.md
+++ README.md
@@ -25,6 +25,53 @@ Download the latest [single header file](https://raw.githubusercontent.com/danie
 
 ## Examples
 
+### A slice object can be constructed with `start`, `stop` and `stride` parameters
+```c++
+int main()
+{
+    ndarray<double,1> a = {0,1,2,3,4,5,6,7,8,9};
+
+    ndarray_view<double, 1> v(a, {slice(2,7,2)});
+    std::cout << v << "\n\n";
+}
+```
+Output:
+```
+[2,4,6]
+```
+
+### A slice object constructed with no parameters selects all the elements along the dimension of an array. 
+```c++
+int main()
+{
+    // Construct a 2-dimensional 3 x 3 array 
+    ndarray<double,2> a = {{1,2,3},{4,5,6},{7,8,9}};
+
+    std::cout << "All items from the second row\n";
+    ndarray_view<double,1> v1(a, {1});
+    std::cout << v1 << "\n\n";
+
+    std::cout << "All items from the second column\n";
+    ndarray_view<double,1> v2(a, {slice()}, {1});
+    std::cout << v2 << "\n\n";
+
+    std::cout << "All items from column 1 onwards\n";
+    ndarray_view<double,2> v3(a, {slice(),slice(1)});
+    std::cout << v3 << "\n\n";
+}
+```
+Output:
+```
+All items from the second row
+[4,5,6]
+
+All items from the second column
+[2,5,8]
+
+All items from column 1 onwards
+[[2,3],[5,6],[8,9]]
+```
+
 ### Row major, zero based indexing example
 
 ```c++
--- examples/src/examples.cpp
+++ examples/src/examples.cpp
@@ -6,10 +6,9 @@ using namespace acons;
 
 void example1()
 {
-    // Construct a 2-dimensional 2 x 2 array 
     ndarray<double,1> a = {0,1,2,3,4,5,6,7,8,9};
 
-    ndarray_view<double, 1> v(a, { slice(2,7,2) });
+    ndarray_view<double, 1> v(a, { slice(2,7,2)});
     std::cout << v << "\n\n";
 
     // [2,4,6]
@@ -69,6 +68,35 @@ void example6()
 
     // [3 4 5]
 }
+
+void example7()
+{
+    // Construct a 2-dimensional 3 x 3 array 
+    ndarray<double,2> a = {{1,2,3},{4,5,6},{7,8,9}};
+
+    std::cout << "All items from the second row\n";
+    ndarray_view<double,1> v1(a, {1});
+    std::cout << v1 << "\n\n";
+
+    std::cout << "All items from the second column\n";
+    ndarray_view<double,1> v2(a, {slice()}, {1});
+    std::cout << v2 << "\n\n";
+
+    std::cout << "All items from column 1 onwards\n";
+    ndarray_view<double,2> v3(a, {slice(),slice(1)});
+    std::cout << v3 << "\n\n";
+}
+
+/*
+All items from the second row
+[4,5,6]
+
+All items from the second column
+[2,5,8]
+
+All items from column 1 onwards
+[[2,3],[5,6],[8,9]]
+*/
 
 void row_major_zero_based_example()
 {
@@ -154,6 +182,9 @@ int main()
     example4();
     example5();
     example6();
+    example7();
+
+    std::cout << "---\n";
 
     row_major_zero_based_example();
     column_major_one_based_example();
